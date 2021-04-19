7f52b76738d6d2690b1fc28ef4420cc4d3a0733b
unknown <97.rramirez@gmail.com>
2021-04-19 03:10:39 -0300

    compiled

--- src/database.ts
+++ src/database.ts
@@ -11,7 +11,9 @@ import config from './config';
             // user: config.MONGO_USER,
             // pass: config.MONGO_PASSWORD 
         }
-        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,mongooseOptions)
+        const db = await mongoose.connect(/* `mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}` */
+        `mongodb+srv://note_db:P0VEpWwj6xuFv1KR@cluster0.hrmen.mongodb.net/test`
+       /*  ,mongooseOptions */)
         console.log('Base de datos conectada a:', db.connection.name)
 
     } catch (error) {
